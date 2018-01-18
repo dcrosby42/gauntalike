7d48f76fe890e2198dc72a9b51d6f8a75066cbc8
David Crosby <dcrosby@llnw.com>
2018-01-18 18:13:02 -0500

    setting up an carsim sandbox module

--- gauntlet.love/modules/carsim/init.lua
+++ gauntlet.love/modules/carsim/init.lua
@@ -1,86 +1,185 @@
-local Module = {}
+local M = {}
 
-local Base = require 'modules.base'
-local BodyDefs = require 'modules.carsim.bodydefs'
-local Scripts = require 'modules.carsim.scripts'
+-- opts
+--   type | ...
+--   body
+--     type  | (dynamic) static kinematic
+--     x | 0
+--     y | 0
+--   shapes[]
+--     type | rectangle circle chain edge (polygon)
+--     x (for circle and rectangle only)
+--     y (for circle and rectangle only)
+--     pts (for edge, chain and polygon)
+--     width (for rectangles only)
+--     height (for rectangles only)
+--     radius (for circles only)
+--     loop (for chain only)
+--     sensor | true (false)
+--     userData | nil
+local function newPhysicsObject(physicsWorld,opts)
+  local obj = {
+    type=opts.type,
+    body={},
+    shape={},
+    fixture={},
+    shapes={},
+    fixtures={},
+  }
 
-local UpdateSystem = composeSystems(requireModules({
-  'systems.timer',
-  'systems.selfdestruct',
-  'systems.controller',
-  -- 'systems.herocontroller',
-  'systems.physics',
-  'systems.collision',
-  -- 'systems.referee',
-  'systems.script',
-}))
+  opts.body = opts.body or {}
+  local b = opts.body
+  b.x = b.x or 0
+  b.y = b.y or 0
+  b.type = b.type or "dynamic"
+  obj.body = love.physics.newBody(physicsWorld, b.x, b.y, b.type)
 
-local DrawSystem = composeDrawSystems(requireModules({
-  'systems.physicsdraw',
-  -- 'modules.dungeon.drawdungeonsystem',
-}))
-
-love.physics.setMeter(64) --the height of a meter our worlds will be 64px
-
-Module.newWorld = Base.makeSetupFunc(function(opts, world)
-  world.resources.caches = {}
-  world.resources.bodyDefs = BodyDefs
-  world.resources.scripts = Scripts
-  -- world.resources.anims = Anims.load()
-  -- world.resources.maps = {} -- Maps.load()
-
-  local estore = world.estore
-
-  local phys = estore:newEntity({
-    {'name',{name="physics world"}},
-    {'physicsWorld', {allowSleep=false, gx=0, gy=0}},
-  })
-
-  phys:newChild({
-    {'name', {name="Car"}},
-    {'controller',{id="kbdController"}},
-    {'script',{script='car',args='',state={keys={}}}},
-    {'body',{kind='car', debugDraw=true}},
-    {'pos', {x=100,y=100}},
-    {'vel', {dx=0,dy=0}},
-    {'force', {fx=0,fy=0}},
-    -- {'controller', {id = pl.id}},
-  })
-
-  -- estore:newEntity({
-  --   {'name',{name="Referee"}},
-  --   {'referee',{}},
-  --   {'controller', {id='referee'}},
-  -- })
-
-  -- Level.addLevel(opts.levelInfo, estore, world.resources)
-end)
-
-
-
-Module.updateWorld = Base.makeUpdateFunc(UpdateSystem, function(world,action,exports)
-  if action.type == 'keyboard' then
-    world.estore:seekEntity(hasComps('script'), function(e)
-      if e.name and e.name.name == 'Car' then
-        table.insert(e.script.state.keys, action)
-        return true
-      end
-    end)
-    -- if action.key == '8' and action.state == 'pressed' then
-    --   world.estore:walkEntities(hasComps('mob'), function(e)
-    --     e.vel.dx = -50
-    --   end)
-    -- end
+  local ss = nil
+  if opts.shapes then
+    ss = opts.shapes
+  elseif opts.shape then
+    ss = {opts.shape}
+  else
+    error("Provide opts.shape or opts.shape")
   end
-  -- (the 'exports' come from the generated portion of the makeUpdateFunc)
-  return world,exports
-end)
 
-Module.drawWorld = Base.makeDrawFunc({
-  -- before=function(world)
-  --   love.graphics.setBackgroundColor(40,40,40)
-  -- end,
-  system=DrawSystem,
-})
+  for _,sh in ipairs(ss) do
+    local shape = nil
+    if sh.type == 'rectangle' then
+      sh.x = sh.x or 0
+      sh.y = sh.y or 0
+      sh.width = sh.width or 0
+      sh.height = sh.height or 0
+      shape = love.physics.newRectangleShape(sh.x,sh.y,sh.width,sh.height,0)
+    elseif sh.type == 'circle' then
+      sh.x = sh.x or 0
+      sh.y = sh.y or 0
+      sh.radius = sh.radius or 1
+      shape = love.physics.newCircleShape(sh.x,sh.y, sh.radius)
+    elseif sh.type == 'chain' then
+      sh.pts = sh.pts or {}
+      shape = love.physics.newChainShape(sh.loop, unpack(sh.pts))
+    elseif sh.type == 'edge' then
+      sh.pts = sh.pts or {}
+      shape = love.physics.newEdgeShape(unpack(sh.pts))
+    elseif sh.type == 'polygon' then
+      sh.pts = sh.pts or {}
+      shape = love.physics.newPolygonShape(sh.pts)
+    else
+      error("Cannot build a phyics shape for sh.type="..tostring(sh.type).." -- "..tdebug(opts))
+    end
+    table.insert(obj.shapes, shape)
 
-return Module
+    local fixture = love.physics.newFixture(obj.body, shape)
+    fixture:setUserData(opts.userData)
+    if sh.sensor == true then
+      fixture:setSensor(true)
+    end
+    if sh.filter then
+      local f = sh.filter
+      if f.cats then
+        fixture:setCategory(unpack(f.cats))
+      end
+      if f.mask then
+        fixture:setMask(unpack(f.mask))
+      end
+      if f.group then
+        fixture:setGroupIndex(f.group)
+      end
+    end
+    if sh.userData ~= nil then
+      fixture:setUserData(sh.userData)
+    end
+    table.insert(obj.fixtures, fixture)
+  end
+
+  if #obj.shapes > 0 then
+    obj.shape = obj.shapes[1]
+    obj.fixture = obj.fixtures[1]
+  end
+
+  if b.mass then
+    obj.body:setMass(b.mass)
+  end
+  if b.linearDamping then
+    obj.body:setLinearDamping(b.linearDamping)
+  end
+  if b.angularDamping then
+    obj.body:setAngularDamping(b.angularDamping)
+  end
+  if b.fixedRotation then
+    obj.body:setFixedRotation(true)
+  end
+
+  return obj
+end
+
+local function updateBodyObject(obj,body,e,estore,input,res)
+  obj.body:setPosition(getPos(e))
+  obj.body:setAngle(e.pos.r)
+  if e.vel then
+    obj.body:setLinearVelocity(e.vel.dx, e.vel.dy)
+  end
+  if e.force then
+    obj.body:applyForce(e.force.fx, e.force.fy)
+  end
+end
+
+local function drawPhysicsShape(body,shape)
+  if shape:type() == "CircleShape" then
+    local x,y = body:getWorldPoints(shape:getPoint())
+    local r = shape:getRadius()
+    love.graphics.circle("line", x,y,r)
+  elseif shape:type() == "ChainShape" then
+    love.graphics.line(body:getWorldPoints(shape:getPoints()))
+  else
+    love.graphics.polygon("line", body:getWorldPoints(shape:getPoints()))
+  end
+  love.graphics.points(body:getWorldPoint(0,0))
+end
+
+--
+--
+--
+
+M.newWorld = function(opts)
+  love.physics.setMeter(1) --the height of a meter our worlds will be 64px
+
+  local world = {}
+  world.physworld = love.physics.newWorld(0,0,false)
+
+
+  local opts = {
+    type="dynamic",
+    body={
+      x=300,
+      y=100,
+    },
+    shape={
+      type='rectangle',
+      width=100,
+      height=60,
+    },
+  }
+  local obj = newPhysicsObject(world.physworld, opts)
+
+  world.obj = obj
+
+
+  return world
+end
+
+M.updateWorld = function(world,action)
+  if action.type == 'tick' then
+    world.physworld:update(action.dt)
+  end
+  return world
+end
+
+M.drawWorld = function(world)
+  love.graphics.setBackgroundColor(0,0,0)
+  local sh = world.obj.shapes[1]
+  drawPhysicsShape(world.obj.body, sh)
+end
+
+return M 
